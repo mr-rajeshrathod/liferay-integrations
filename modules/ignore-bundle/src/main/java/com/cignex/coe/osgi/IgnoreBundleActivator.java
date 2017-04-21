aeeb187042621105852d89bf392d765b5f6d498a
Meet <consultant.meet@gmail.com>
2017-04-21 18:05:35 +0530

    Sample OSGI bundle for example.

---
+++ modules/ignore-bundle/src/main/java/com/cignex/coe/osgi/IgnoreBundleActivator.java
@@ -0,0 +1,19 @@
+package com.cignex.coe.osgi;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * @author rajesh.rathod
+ */
+public class IgnoreBundleActivator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext bundleContext) throws Exception {
+	}
+
+	@Override
+	public void stop(BundleContext bundleContext) throws Exception {
+	}
+
+}
